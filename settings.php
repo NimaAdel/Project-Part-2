2f1eb73d657a4431c89fff5a7dd10d5c5e362f31
105337923 <105337923@student.swin.edu.au>
2025-10-21 09:25:34 +1100

    Remove no longer relevant comment

--- settings.php
+++ settings.php
@@ -1,9 +1,9 @@
 <?php
 // Database credentials for local server
 define('DB_HOST', 'localhost');
-define('DB_USER', 'root'); // As specified, no password for local dev
+define('DB_USER', 'root');
 define('DB_PASSWORD', '');
-define('DB_NAME', 'test'); // Using 'jobsdb' for all operations for simplicity
+define('DB_NAME', 'test');
 
 // Establish database connection
 $conn = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
