3b50cc0a0cf2a8432a532d146835266f3d07aed3
105337923 <105337923@student.swin.edu.au>
2025-10-21 09:42:27 +1100

    Properly commenting my code to be able to help better explain it.

--- settings.php
+++ settings.php
@@ -1,9 +1,9 @@
 <?php
 // Database credentials for local server
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
+define('DB_HOST', 'localhost'); //This line establishes where the databse is located. If moved into production this would need to be changed
+define('DB_USER', 'root'); // These credentials are simply for accenessing the pages, hence why they do not need a password. These are not the credentials used to log in. However given database access is open, any passwords stored in the database should be properly hashed
 define('DB_PASSWORD', '');
-define('DB_NAME', 'test');
+define('DB_NAME', 'test'); // NOTE: WE SHOULD PROBABLY NOT CALL THE DATABASE TEST WHEN SUBMITTING
 
 // Establish database connection
 $conn = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
