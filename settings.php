1bf578117d48c83418c7e990c5007d5b5a084fd2
105337923 <105337923@student.swin.edu.au>
2025-10-27 06:48:59 +1100

    Changed test.sql to database.sql for submission

--- settings.php
+++ settings.php
@@ -3,7 +3,7 @@
 define('DB_HOST', 'localhost'); //This line establishes where the databse is located. If moved into production this would need to be changed
 define('DB_USER', 'root'); // These credentials are simply for accenessing the pages, hence why they do not need a password. These are not the credentials used to log in. However given database access is open, any passwords stored in the database should be properly hashed
 define('DB_PASSWORD', '');
-define('DB_NAME', 'test'); // NOTE: WE SHOULD PROBABLY NOT CALL THE DATABASE TEST WHEN SUBMITTING
+define('DB_NAME', 'database');
 
 // Establish database connection
 $conn = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
@@ -11,7 +11,7 @@ $conn = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
-}else {
+} else {
     // Optional message for testing
     // echo "<p style='color:green;'>Database connection successful!</p>";
 }
